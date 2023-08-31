90ab1d5c287321db2720d750acc89169bc51dcc4
Shahariar Sohan <Sohanshahariar4@gmail.com>
2023-08-31 09:45:31 +0600

    ai card add complete

--- index.js
+++ index.js
@@ -16,18 +16,20 @@ const aiCardLoad = (details) => {
   details.forEach(detail => {
     console.log(detail)
     const aiCard = document.createElement('div')
-    aiCard.classList = "card bg-base-100 shadow-xl my-10"
+    aiCard.classList = "card bg-base-100 shadow-xl my-5 "
     aiCard.innerHTML = `
-        <figure class="px-10 pt-10">
-        <img src="${detail?.image}" alt="ai" class="rounded-xl" />
+        <figure class="w-[80%] h-[180px] mx-auto">
+        <img src="${detail?.image}" alt="ai" class="rounded-xl w-full h-full" />
       </figure>
-      <div class="card-body ">
+      <div class=" w-[80%] h-[180px] mx-auto my-10">
         <h2 class="text-xl font-bold">Features</h2>
         <p class="text-md text-gray-400 ">1. ${detail?.features[0]}</p>
         <p class="text-md text-gray-400 ">2. ${detail?.features[1]}</p>
         <p class="text-md text-gray-400 ">3. ${detail?.features[2]}</p>
-        <div class="card-actions">
+        <div class="w-full h-[1px] bg-slate-200 my-5">
         </div>
+        <p class="text-xl font-bold">${detail?.name}</p>
+        <p class="text-md text-gray-400 ">Published In: ${detail?.published_in}</p>
       </div>
         `
     aiCardContainer.appendChild(aiCard)
