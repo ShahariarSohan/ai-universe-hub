3f8c22057d02f83f1035d9aca4d775c7c728b78e
Shahariar Sohan <Sohanshahariar4@gmail.com>
2023-08-31 09:01:32 +0600

    features added

--- index.js
+++ index.js
@@ -1,32 +1,35 @@
+//ai data load
 const aiDataLoad = async () => {
-    const res = await fetch(`https://openapi.programming-hero.com/api/ai/tools`)
-    const data = await res.json()
-    const details = data.data.tools
-    console.log(details)
-    aiCardLoad(details)
+  const res = await fetch(`https://openapi.programming-hero.com/api/ai/tools`)
+  const data = await res.json()
+  const details = data.data.tools
+  console.log(details)
+  aiCardLoad(details)
 }
 aiDataLoad()
+//ai card load
 const aiCardLoad = (details) => {
-    const aiCardContainer = document.getElementById('ai-card-container')
-    aiCardContainer.textContent = ''
-    details = details.slice(0, 9)
-    console.log(details.length)
-    details.forEach(detail => {
-        console.log(detail)
-        const aiCard = document.createElement('div')
-        aiCard.classList = "card bg-base-100 shadow-xl my-10"
-        aiCard.innerHTML = `
+  const aiCardContainer = document.getElementById('ai-card-container')
+  aiCardContainer.textContent = ''
+  details = details.slice(0, 9)
+  console.log(details.length)
+  details.forEach(detail => {
+    console.log(detail)
+    const aiCard = document.createElement('div')
+    aiCard.classList = "card bg-base-100 shadow-xl my-10"
+    aiCard.innerHTML = `
         <figure class="px-10 pt-10">
         <img src="${detail?.image}" alt="ai" class="rounded-xl" />
       </figure>
-      <div class="card-body items-center text-center">
-        <h2 class="card-title">Shoes!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
+      <div class="card-body ">
+        <h2 class="text-xl font-bold">Features</h2>
+        <p class="text-md text-gray-400 ">1. ${detail?.features[0]}</p>
+        <p class="text-md text-gray-400 ">2. ${detail?.features[1]}</p>
+        <p class="text-md text-gray-400 ">3. ${detail?.features[2]}</p>
         <div class="card-actions">
-          <button class="btn btn-primary">Buy Now</button>
         </div>
       </div>
         `
-        aiCardContainer.appendChild(aiCard)
-    });
+    aiCardContainer.appendChild(aiCard)
+  });
 }
