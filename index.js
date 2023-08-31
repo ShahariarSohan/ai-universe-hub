492c2fd5431f8ad43d9ef7670f40e935b7c75fbf
Shahariar Sohan <Sohanshahariar4@gmail.com>
2023-08-31 11:43:57 +0600

    card modified

--- index.js
+++ index.js
@@ -15,13 +15,13 @@ const aiCardLoad = (details) => {
   console.log(details.length)
   details.forEach(detail => {
     // console.log(detail)
-    const aiCard = document.createElement('div')
-    aiCard.classList = "card bg-base-100 shadow-xl my-5 "
-    aiCard.innerHTML = `
-        <figure onclick="aiModalData('${detail.id}')" class="w-[80%] h-[180px] mx-auto">
+    const aiCardDiv = document.createElement('div')
+    aiCardDiv.innerHTML = `
+        <div onclick="aiModalData('${detail.id}')"  class="card bg-base-100  shadow-xl p-5">
+        <figure class="w-[100%] h-[180px] ">
         <img src="${detail?.image}" alt="ai" class="rounded-xl w-full h-full" />
       </figure>
-      <div onclick="aiModalData('${detail.id}')" class=" w-[80%] h-[180px] mx-auto my-10">
+      <div class=" w-[100%] h-[180px]  my-5 ">
         <h2 class="text-xl font-bold">Features</h2>
         <p class="text-md text-gray-400 ">1. ${detail?.features[0]}</p>
         <p class="text-md text-gray-400 ">2. ${detail?.features[1]}</p>
@@ -31,8 +31,9 @@ const aiCardLoad = (details) => {
         <p class="text-xl font-bold">${detail?.name}</p>
         <p class="text-md text-gray-400 ">Published In: ${detail?.published_in}</p>
       </div>
+      </div>
         `
-    aiCardContainer.appendChild(aiCard)
+    aiCardContainer.appendChild(aiCardDiv)
   });
 }
 //ai modal data
@@ -40,4 +41,5 @@ const aiModalData = async (id) => {
   const res = await fetch(`https://openapi.programming-hero.com/api/ai/tool/${id}`)
   const data = await res.json()
   console.log(data.data)
+  ai_modal.showModal()
 }
